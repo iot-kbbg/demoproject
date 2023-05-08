8a5bc43899bbe1b61c4ef51de3dba685479e7e17
goossensbas <goossensbas@gmail.com>
2023-05-08 10:43:45 +0200

    added wifi led status + blink on sensor read

--- read_values.py
+++ read_values.py
@@ -1,9 +1,36 @@
 import machine
+import network
 import time
 from machine import I2C
-bus = I2C(1, scl = machine.Pin(22), sda = machine.Pin(21))
 from bmp280 import *
+from utils import led_blink
+
+# Define Led pins
+LED_R = machine.Pin(16, machine.Pin.OUT)
+LED_G = machine.Pin(17, machine.Pin.OUT)
+LED_B = machine.Pin(18, machine.Pin.OUT)
+
+# init I2C
+bus = I2C(1, scl = machine.Pin(22), sda = machine.Pin(21))
 bmp = BMP280(bus)
+
+# wifi credentials
+iot_ssid = 'bvnetwerk'
+iot_passwd = 'xLbCMeZtac'
+
+# Enable wifi
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+
 while (True):
-    print(bmp.temperature)
-    time.sleep (30)
+    if sta_if.isconnected():
+        print(sta_if.ifconfig())
+        LED_R.value(1)
+        print(bmp.temperature)
+        led_blink(LED_G, 1)
+        time.sleep (30)
+    else:
+        sta_if.connect(iot_ssid, iot_passwd)
+        LED_R.value(0)
+        while not sta_if.isconnected():
+            pass
